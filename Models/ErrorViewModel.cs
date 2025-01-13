075a2db1c24b92146d2ada8697118b4b802c3a0d
Benja <benjaminvilla409@gmail.com>
2025-01-12 22:30:10 -0300

    Proyecto iniciado y creacion de la BD

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace tl2_proyecto_2024_Daggam.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
