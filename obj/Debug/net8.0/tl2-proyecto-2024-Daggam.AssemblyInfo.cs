4356176b0324eb8e734135dc8021fd29ca55f5bd
Benja <benjaminvilla409@gmail.com>
2025-01-23 16:38:47 -0300

    Vistas de usuarios terminadas

--- obj/Debug/net8.0/tl2-proyecto-2024-Daggam.AssemblyInfo.cs
+++ obj/Debug/net8.0/tl2-proyecto-2024-Daggam.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tl2-proyecto-2024-Daggam")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e3b6b44499e46553ff732194ab100d171794123")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cecb01faff9870b4f42a82e3ff8361139bb5d796")]
 [assembly: System.Reflection.AssemblyProductAttribute("tl2-proyecto-2024-Daggam")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tl2-proyecto-2024-Daggam")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
